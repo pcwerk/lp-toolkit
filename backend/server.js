77f10a75f746b804a4dc06bbd4799826c696dd25
shant01 <shanthovagimian@gmail.com>
2023-10-09 15:02:12 -0700

    Updated Import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import serverHealthRoutes from "./routes/serverHealthRoutes";
+import serverHealthRoutes from "./routes/serverHealthRoutes.js";
 
 const app = express();
 
