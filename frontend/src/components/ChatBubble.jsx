e50ada12031194a8fb7fb8cccdec5a4e2cba57c0
Arturo <chiefchurro@gmail.com>
2023-10-12 14:22:08 -0700

    added icons to user and response

--- frontend/src/components/ChatBubble.jsx
+++ frontend/src/components/ChatBubble.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsFillPersonFill } from "react-icons/bs";
+import { AiOutlineRobot } from "react-icons/ai";
 
 export function ChatBubble({ message, response, index }) {
   return (
@@ -7,12 +9,14 @@ export function ChatBubble({ message, response, index }) {
       className="my-2 p-3 rounded-lg ml-auto bg-gray-100 text-black shadow-md"
     >
       <div //User input
-        className="p-2 mb-2 rounded-lg bg-gray-100 text-black "
+        className="p-2 mb-2 rounded-lg bg-gray-100 text-black flex items-center"
       >
+        <BsFillPersonFill size={22} className=" mr-4" />
         {message.content}
       </div>
       {response && (
-        <div className="p-2 rounded-lg bg-white text-black">
+        <div className="p-2 rounded-lg bg-white text-black flex items-center">
+          <AiOutlineRobot size={22} className=" mr-4" />
           {response.content}
         </div>
       )}
