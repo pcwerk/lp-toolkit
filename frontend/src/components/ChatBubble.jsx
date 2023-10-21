35e492787ef3f11ca7ae7f57de7a3d57f05a3bfb
Arturo <chiefchurro@gmail.com>
2023-10-21 16:04:45 -0700

    dark mode for chat bubble

--- frontend/src/components/ChatBubble.jsx
+++ frontend/src/components/ChatBubble.jsx
@@ -6,17 +6,17 @@ export function ChatBubble({ message, response, index }) {
   return (
     <div //Outer box
       key={index}
-      className="my-2 p-3 rounded-lg ml-auto bg-gray-100 text-black shadow-md"
+      className="my-2 p-3 rounded-lg ml-auto bg-gray-100 dark:bg-[#444A4B] text-black shadow-md"
     >
-      <div //User input
-        className="p-2 mb-2 rounded-lg bg-gray-100 text-black flex items-center"
+      <div //User input ,no background similar
+        className="p-2 mb-2 rounded-lg text-black dark:text-gray-200 flex items-center"
       >
-        <BsFillPersonFill size={24} className=" mr-4" />
+        <BsFillPersonFill size={24} className=" mr-4 dark:text-gray-400" />
         {message.content}
       </div>
       {response && (
-        <div className="p-2 rounded-lg bg-white text-black flex items-center">
-          <AiOutlineRobot size={24} className=" mr-4" />
+        <div className="p-2 rounded-lg bg-white text-black flex items-center dark:bg-[#131314] dark:text-gray-200">
+          <AiOutlineRobot size={24} className=" mr-4 dark:text-gray-400" />
           {response.content}
         </div>
       )}
