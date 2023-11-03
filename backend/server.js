94cc3bec42d0ee148fb203ed0fc1ca594739f21e
shant01 <shanthovagimian@gmail.com>
2023-11-03 10:11:44 -0700

    Setting up docker-compose and mongo locally:

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const port = process.env.PORT || 5050;
-
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/lp-toolkit'
+console.log("MONGO URI" , mongoURI)
 app.use('/serverHealthRoutes', serverHealthRoutes);
 
 // This displays a message indicating that the server is running and listening to the specified port
