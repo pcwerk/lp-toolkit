895540708c183ef699d412a1f9fa2800d9c030dd
Arturo <chiefchurro@gmail.com>
2023-11-03 19:35:52 -0700

    langchain agent file defined

--- langchain/app/main.py
+++ langchain/app/main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
-
+#Create the FastAPI app
+from fastapi import FastAPI 
 app = FastAPI()
+
+#pydantic for data validation
+from pydantic import BaseModel
+class InputData(BaseModel):
+    text: str
+
+
+#Endpoint for text input
+@app.post("/process-lang")
+async def process_lang(data: InputData): #Take in the data as param
+    #Call OpenAI API, APIKEY
+    
+
+
+
 
 #test to see if api works
 @app.get("/")
