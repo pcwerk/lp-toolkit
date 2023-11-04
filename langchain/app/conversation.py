6f4a6f0637c3e70c2324ef90424380ec5e7c1138
Arturo <chiefchurro@gmail.com>
2023-11-03 19:26:07 -0700

    conversation file for langchain, main for api

--- langchain/app/conversation.py
+++ langchain/app/conversation.py
@@ -1,1 +1,12 @@
 #Logic for langchain
+
+#Import requirements
+from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
+
+#Load in the enviroment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+#create function to recieve the input from user
+def conversation(human_input):
+    template = "You are a helpful assistant who"
--- langchain/app/main.py
+++ langchain/app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#test to see if api works
 @app.get("/")
 def home():
     return {"Data": "Testing"}
+
+#route for the conversation
+@app.post("/conversation")
+async def input():
+    return {"Conversation": "Test"}
