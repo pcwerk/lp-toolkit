60e569edfa754de4d5e19bcdad81d4918c21430b
Arturo <chiefchurro@gmail.com>
2023-11-06 15:18:02 -0800

    langchaincallroute finished

--- backend/routes/langchainCallRoute.js
+++ backend/routes/langchainCallRoute.js
@@ -1,9 +1,29 @@
 import express from "express";
-
 const langchainCallRoute = express.Router();
+import axios from "axios";
 
-//post to post to the fastapi langchain api
-// Basic route to return a timestamp
-langchainCallRoute.post("/langchainChat", (req, res) => {
+langchainCallRoute.post("/ask", async (req, res) => {
   //call fastapi from this endpoint
+  try {
+    //get user input
+    const userInput = "What are some good soccer team names?";
+
+    //make request to fastapi server
+    const fastResponse = await axios.post(
+      "http://localhost:8000/process-lang",
+      {
+        human_input: userInput,
+      }
+    );
+
+    //response from fastapi
+    const dataFromFastapi = fastResponse.data;
+
+    res.json({ message: "Successful call to FastAPI", dataFromFastapi });
+  } catch (error) {
+    console.error("Error calling FastAPI", error);
+    res.status(500).json({ error: "Failed to call FastAPI" });
+  }
 });
+
+export default langchainCallRoute;
--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors(corsOptions));
 const port = process.env.PORT || 5050;
 
 app.use("/serverHealthRoutes", serverHealthRoutes);
-app.use("/call-langchain", langchainCallRoute);
+app.use("/langchainCallRoute", langchainCallRoute);
 
 // This displays a message indicating that the server is running and listening to the specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
