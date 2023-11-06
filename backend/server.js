d731e71579030f157914b692b8e182dbc4714857
Arturo <chiefchurro@gmail.com>
2023-11-06 14:36:14 -0800

    added langchaincall route to server file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import serverHealthRoutes from "./routes/serverHealthRoutes.js";
+import langchainCallRoute from "./routes/langchainCallRoute.js";
 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(cors(corsOptions));
 
 const port = process.env.PORT || 5050;
 
-app.use('/serverHealthRoutes', serverHealthRoutes);
+app.use("/serverHealthRoutes", serverHealthRoutes);
+app.use("/call-langchain", langchainCallRoute);
 
 // This displays a message indicating that the server is running and listening to the specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
