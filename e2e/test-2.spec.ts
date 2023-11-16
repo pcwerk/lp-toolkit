677eab9a5357f2656ba9430edb10ae31a8c9e872
shant01 <shanthovagimian@gmail.com>
2023-11-15 19:07:26 -0800

    Update

--- e2e/test-2.spec.ts
+++ e2e/test-2.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('http://localhost:3000/');
+  await page.getByPlaceholder('Type your message...').click();
+  await page.getByPlaceholder('Type your message...').fill('How do I setup playwright in my js project');
+  await page.getByRole('button', { name: 'Send' }).click();
+
+   
 });
