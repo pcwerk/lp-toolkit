10e93804e85ac26c40b9093e495d10493a6fb361
Arturo <chiefchurro@gmail.com>
2024-04-18 19:31:48 -0700

    loading local model

--- huggingface/localmodel.py
+++ huggingface/localmodel.py
@@ -1,8 +1,16 @@
 #Import Modules
 from dotenv import load_dotenv
 from huggingface_hub import hf_hub_download
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+
+#Model Loading
+hf = HuggingFacePipeline.from_model_id(
+    model_id='gpt2',
+    task='text=generation',
+    pipeline_kwargs={'max_new_tokens': 10},
+)
 
 
-#Load Env variable
-load_dotenv()
+
 
