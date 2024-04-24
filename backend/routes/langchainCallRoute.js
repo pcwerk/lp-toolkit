7d3170d0459f508bfd62f8a00fa9287bb96a7e82
Arturo <chiefchurro@gmail.com>
2024-04-23 23:07:50 -0700

    created endpoint for express

--- backend/routes/langchainCallRoute.js
+++ backend/routes/langchainCallRoute.js
@@ -26,4 +26,29 @@ langchainCallRoute.post("/ask", async (req, res) => {
   }
 });
 
+//Endpoint for huggingfacecall
+langchainCallRoute.post("/askhf", async (req, res) => {
+  //call fastapi from this endpoint
+  try {
+    //get user input
+    const userInput = req.body.data;
+
+    //make request to fastapi server
+    const fastResponse = await axios.post(
+      "http://langchain:8000/hfprocesstext",
+      {
+        human_input: userInput,
+      }
+    );
+
+    //response from fastapi
+    const dataFromFastapi = fastResponse.data;
+
+    res.json({ message: "Successful call to FastAPI", dataFromFastapi });
+  } catch (error) {
+    console.error("Error calling FastAPI", error);
+    res.status(500).json({ error: "Failed to call FastAPI" });
+  }
+});
+
 export default langchainCallRoute;
